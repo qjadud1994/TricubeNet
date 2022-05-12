8da4efa833f84e8f3248a5491960871dfc1a9fa1
Beom <qjadud1994@gmail.com>
2022-05-12 15:13:31 +0900

    [fix] error in lr_scheduler.py

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -1,35 +1,114 @@
+import torch
+import torch.optim as optim
+
+import math
+import numpy as np
 from bisect import bisect_right
-from torch.optim.lr_scheduler import _LRScheduler
-    
-def Warmup_LR_scheduler(optimizer, milestones, 
-                        gamma=0.1, warmup_factor=1.0 / 3, 
-                        warmup_iters=1000, last_epoch=-1):
-    
+from typing import List
 
-    class WarmupMultiStepLR(_LRScheduler):
-        def __init__(self, optimizer, milestones, gamma, warmup_factor, warmup_iters, last_epoch):
-            if not list(milestones) == sorted(milestones):
-                raise ValueError(
-                    "Milestones should be a list of" " increasing integers. Got {}",
-                    milestones,
-                )
-
-            self.milestones = milestones
-            self.gamma = gamma
-            self.warmup_factor = warmup_factor
-            self.warmup_iters = warmup_iters
-            super().__init__(optimizer, last_epoch)
-
-        def get_lr(self):
-            warmup_factor = 1
-            if self.last_epoch < self.warmup_iters:
-                alpha = float(self.last_epoch) / self.warmup_iters
-                warmup_factor = self.warmup_factor * (1 - alpha) + alpha
-            return [
-                base_lr
-                * warmup_factor
-                * self.gamma ** bisect_right(self.milestones, self.last_epoch)
-                for base_lr in self.base_lrs
-            ]
     
-    return WarmupMultiStepLR(optimizer, milestones, gamma, warmup_factor, warmup_iters, last_epoch)
+class WarmupPolyLR(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        max_iters: int,
+        warmup_factor: float = 0.001,
+        warmup_iters: int = 1000,
+        warmup_method: str = "linear",
+        last_epoch: int = -1,
+        power: float = 0.9,
+        constant_ending: float = 0.
+    ):
+        self.max_iters = max_iters
+        self.warmup_factor = warmup_factor
+        self.warmup_iters = warmup_iters
+        self.warmup_method = warmup_method
+        self.power = power
+        self.constant_ending = constant_ending
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self) -> List[float]:
+        warmup_factor = _get_warmup_factor_at_iter(
+            self.warmup_method, self.last_epoch, self.warmup_iters, self.warmup_factor
+        )
+        if self.constant_ending > 0 and warmup_factor == 1.:
+            # Constant ending lr.
+            if math.pow((1.0 - self.last_epoch / self.max_iters), self.power) < self.constant_ending:
+                return [
+                    base_lr
+                    * self.constant_ending
+                    for base_lr in self.base_lrs
+                ]
+        return [
+            base_lr
+            * warmup_factor
+            * math.pow((1.0 - self.last_epoch / self.max_iters), self.power)
+            for base_lr in self.base_lrs
+        ]
+
+    def _compute_values(self) -> List[float]:
+        # The new interface
+        return self.get_lr()
+
+    
+class WarmupMultiStepLR(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        milestones: List[int],
+        gamma: float = 0.1,
+        warmup_factor: float = 0.001,
+        warmup_iters: int = 1000,
+        warmup_method: str = "linear",
+        last_epoch: int = -1,
+    ):
+        if not list(milestones) == sorted(milestones):
+            raise ValueError(
+                "Milestones should be a list of" " increasing integers. Got {}", milestones
+            )
+        self.milestones = milestones
+        self.gamma = gamma
+        self.warmup_factor = warmup_factor
+        self.warmup_iters = warmup_iters
+        self.warmup_method = warmup_method
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self) -> List[float]:
+        warmup_factor = _get_warmup_factor_at_iter(
+            self.warmup_method, self.last_epoch, self.warmup_iters, self.warmup_factor
+        )
+        return [
+            base_lr * warmup_factor * self.gamma ** bisect_right(self.milestones, self.last_epoch)
+            for base_lr in self.base_lrs
+        ]
+
+    def _compute_values(self) -> List[float]:
+        # The new interface
+        return self.get_lr()
+
+    
+def _get_warmup_factor_at_iter(
+    method: str, iter: int, warmup_iters: int, warmup_factor: float
+) -> float:
+    """
+    Return the learning rate warmup factor at a specific iteration.
+    See https://arxiv.org/abs/1706.02677 for more details.
+    Args:
+        method (str): warmup method; either "constant" or "linear".
+        iter (int): iteration at which to calculate the warmup factor.
+        warmup_iters (int): the number of warmup iterations.
+        warmup_factor (float): the base warmup factor (the meaning changes according
+            to the method used).
+    Returns:
+        float: the effective warmup factor at the given iteration.
+    """
+    if iter >= warmup_iters:
+        return 1.0
+
+    if method == "constant":
+        return warmup_factor
+    elif method == "linear":
+        alpha = iter / warmup_iters
+        return warmup_factor * (1 - alpha) + alpha
+    else:
+        raise ValueError("Unknown warmup method: {}".format(method))
